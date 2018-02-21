177729ec6731ca164a3780979c0bb944fee0e3e7
Estudiantes <Estudiantes@Labing70204>
2018-02-21 16:47:03 -0500

    Iniciando

---
+++ Vehiculos/src/vehiculos/Moto.java
@@ -0,0 +1,20 @@
+
+package vehiculos;
+
+    
+public class Moto extends Vehiculo {
+
+  @Override
+   void acelerar() {
+
+        System.out.println("Estoy acelerando");
+    }
+
+    @Override
+    void frenar() {
+
+        System.out.println("Estoy frenando");
+    }
+
+    
+}
---
+++ Vehiculos/src/vehiculos/Principal.java
@@ -0,0 +1,10 @@
+
+package vehiculos;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
